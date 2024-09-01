97b45c6bbc06abf24ce3b83b33d1a5cdd09b96f3
Ahmed Abdulrahman <104634941+abdurrahmanu@users.noreply.github.com>
2024-09-01 06:49:05 +0100

    Update Skill.vue

--- client/components/BaseComponents/Skill.vue
+++ client/components/BaseComponents/Skill.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-3 space-y-3 text-sm rounded-md transition-all duration-150 ring-[1px] shadow-md shadow-black hover:shadow-sm z-[2] bg-[#121212] ring-yellow-900">
         <div class="flex justify-between">
-            <div @click="goToProjects" class="flex items-center gap-3 px-3 py-1 rounded-md ring-[1px] ring-neutral-800 hover:ring-yellow-900 hover:text-green-300 h-fit">
+            <div class="flex items-center gap-3 px-3 py-1 rounded-md ring-[1px] ring-neutral-800 hover:ring-yellow-900 hover:text-green-300 h-fit">
                 <img :src="'linkedin.svg' || svg + '.svg'" class="self-start w-3" />
             </div>
         </div>
@@ -17,24 +17,4 @@ const props = defineProps({
     skillInfo: String,
     index: Number,
 });
-
-const filterProjects = () => {
-    console.log(props.skillName)
-}
-
 </script>
-
-<style scoped>
-.skill {
-    @apply pt-3 pl-3 pr-5 text-[10px] border pb-3 rounded-[10px] shadow-lg flex flex-col bg-neutral-800
-}
-
-.link {
-    @apply flex items-center gap-[17px]
-}
-
-.info {
-    @apply max-w-[549px] font-normal
-}
-</style>
-
