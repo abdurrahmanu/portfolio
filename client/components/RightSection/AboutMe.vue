5e52e5ec4149ba562838cf1bd2e84fea04c90610
Ahmed Abdulrahman <104634941+abdurrahmanu@users.noreply.github.com>
2024-09-01 06:39:56 +0100

    Update AboutMe.vue

--- client/components/RightSection/AboutMe.vue
+++ client/components/RightSection/AboutMe.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="about" class="relative ring-[1px] rounded-md ring-green-700 intersect bg-[#121212]" :class="[scrolledInSection !== 'about' ? 'opacity-5 blur-[2px]' : '']">
         <div class="flex">            
-            <img loading="lazy" id="first" @click="current = 'first'" :class="[current === 'first' ? 'over-image' : 'under-image']" class="image border-r-[1px] border-r-black left-0" src="/image.png" alt="">
-            <img loading="lazy" id="second" @click="current = 'second'" :class="[current === 'second' ? 'over-image' : 'under-image']" class="image border-l-[1px] border-l-black absolute right-0" src="/image.png" alt="">
+            <img loading="lazy" id="first" @click="current = 'first'" :class="[current === 'first' ? 'z-[3]' : 'bottom-0 z-[2] opacity-[.1]']" class="image border-r-[1px] border-r-black left-0" src="/image.png" alt="">
+            <img loading="lazy" id="second" @click="current = 'second'" :class="[current === 'second' ? 'z-[3]' : 'bottom-0 z-[2] opacity-[.1]']" class="image border-l-[1px] border-l-black absolute right-0" src="/image.png" alt="">
         </div>
     </div>
     <div v-if="scrolledInSection === 'about'" class="flex justify-center gap-2 pt-3">
@@ -21,13 +21,5 @@ const {scrolledInSection} = storeToRefs(main)
 <style scoped>
 .image {
     @apply w-[70%] bg-neutral-900 rounded-md h-fit max-h-[400px]
-}
-
-.over-image {
-    @apply z-[3] 
-}
-
-.under-image {
-    @apply top-0 bottom-0 z-[2] opacity-[.1]
 }
 </style>
