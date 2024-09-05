4c8589494967b9537e59966044f4d813bd667024
Abdur-Rahman <ahmadabdulrahma103@gmail.com>
2024-09-05 14:20:07 +0100

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  // vite: {
-  //   server: {
-  //     watch: {
-  //       usePolling: true,
-  //     },
-  //   },
-  // },
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
-  // pinia: {
-  //   storesDirs: ['./stores/**', './custom-folder/stores/**'],
-  // },
   spaLoadingTemplate: './loader.html',
 })
